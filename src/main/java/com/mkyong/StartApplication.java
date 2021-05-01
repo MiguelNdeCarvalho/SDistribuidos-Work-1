256a63d4e02b5e0883e054ca69ba28e1660aa227
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-05-01 13:38:55 +0100

    StartApplication: Add an example

--- src/main/java/com/mkyong/StartApplication.java
+++ src/main/java/com/mkyong/StartApplication.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,6 +18,8 @@ public class StartApplication implements CommandLineRunner {
     private CentroRepository centroRepository;
     @Autowired
     private InscricaoRepository inscricaoRepository;
+    @Autowired
+    private VacinadoRepository vacinadoRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
@@ -26,16 +30,23 @@ public class StartApplication implements CommandLineRunner {
 
         log.info("StartApplication...");
 
-        //centroRepository.save(new Centro("Centro de Reguengos de Monsaraz", "Reguengos de Monsaraz"));
-        //centroRepository.save(new Centro("Centro de Monsaraz", "Monsaraz"));
-        //centroRepository.save(new Centro("Centro de Évora", "Évora"));
+        centroRepository.save(new Centro("Centro de Reguengos de Monsaraz", "Reguengos de Monsaraz"));
+        centroRepository.save(new Centro("Centro de Monsaraz", "Monsaraz"));
+        centroRepository.save(new Centro("Centro de Évora", "Évora"));
 
         //List<Centro> queryList = centroRepository.findByRegiaoContainingIgnoreCase("Monsaraz");
         //System.out.println("Tamanho da lista: " + queryList.size() + "\nList: " + queryList.toString());
 
-        //Centro tempCentro = centroRepository.findOneByNomeContainingIgnoreCase("Évora");
-        //inscricaoReposccccccccitory.save(new Inscricao(tempCentro, "C101", "Miguel Carvalho", "Masculino", (long) 20));
-        //System.out.println(tempCentro.toString());
+        Centro centroEvora = centroRepository.findOneByNomeContainingIgnoreCase("Évora");
+        inscricaoRepository.save(new Inscricao(centroEvora, "C101", "Miguel Carvalho", "Masculino", (long) 20));
+        
+        Inscricao miguelInscricao = inscricaoRepository.findOneByNomeContainingIgnoreCase("Miguel Carvalho");
+        
+        LocalDateTime date = LocalDateTime.now();
+
+        vacinadoRepository.save(new Vacinado(miguelInscricao.getCodigo(), miguelInscricao.getNome(), miguelInscricao.getGenero(), miguelInscricao.getIdade(), date, "teste"));
+        
+        inscricaoRepository.deleteById(miguelInscricao.getId());
     }
 
 }

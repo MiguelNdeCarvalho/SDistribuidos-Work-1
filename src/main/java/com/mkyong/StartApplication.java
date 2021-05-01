a35da49c4098b699ec7a93ec7d4c95620845ab23
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-05-01 13:49:14 +0100

    StartApplication.java: Remove unused import

--- src/main/java/com/mkyong/StartApplication.java
+++ src/main/java/com/mkyong/StartApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @SpringBootApplication
 public class StartApplication implements CommandLineRunner {
